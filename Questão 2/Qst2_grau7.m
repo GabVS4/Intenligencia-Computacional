aa6e12c0cc847d86be43d9b08a7270e71c9eaa38
Gabriel Santos <gabrielvs2003@outlook.com>
2022-10-18 12:28:55 -0300

    Segunda Questao

---
+++ Questão 2/Qst2_grau7.m
@@ -0,0 +1,26 @@
+base = load("aerogerador.dat"); %carregando os dados do problema
+
+x = base(:, 1); %velocidade do vento
+y = base(:, 2); %potência
+
+n = length(x); %número de amostras
+
+X = [ones(n, 1) x x.^2 x.^3 x.^4 x.^5 x.^6 x.^7]; %vetor dos valores da velocidade do vento
+
+beta = ((X' * X) \ (X' * y));
+
+%funcão de regressão múltipla
+Y = beta(1) + beta(2) * x + beta(3) * x.^2 + beta(4) * x.^3 + beta(5) * x.^4 + beta(6) * x.^5 + beta(7) * x.^6 + beta(8) * x.^7;
+
+%Mostragem dos dados
+plot(x, y, '*', x, Y, 'r-');
+
+R2 = 1 - (sum((y - Y).^2)) / (sum((y - mean(y)).^2)); %coeficiente de determinação
+fprintf('R2 = %f\n', R2);
+
+[L, C] = size(X); %número de linhas e colunas
+k = C - 1; %número de termos
+p = k + 1;
+
+R2_aj = 1 - ((sum((y - Y).^2)) / (n - p)) / ((sum((y - mean(y)).^2)) / (n-1)); %coeficiente de determinação ajustado
+fprintf('R2 ajustado = %f\n', R2_aj);
