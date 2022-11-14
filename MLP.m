6a1372d495d1b4d305aad471456f5777f88bea98
Gabriel Santos <gabrielvs2003@outlook.com>
2022-11-13 21:34:48 -0300

    Multi Layer Perceptron

---
+++ MLP.m
@@ -0,0 +1,84 @@
+%% Lendo os arquivos:
+arquivo = fopen('column_3C.dat'); 
+colunasArquivo = textscan(arquivo, '%f %f %f %f %f %f %s', 'Delimiter',',');
+
+%% Codifica as classes de saída
+N = length(colunasArquivo{7});
+rotulosUnicos = unique(colunasArquivo{7});
+colunasDeRotulos = zeros(N, length(rotulosUnicos)); %N linhas e 3 colunas
+
+for i = 1: N %Percorre os rótulas para cada amostra
+    for j = 1 : length(rotulosUnicos)
+        colunasDeRotulos(i, j) = strcmp(colunasArquivo{7}{i}, rotulosUnicos(j));
+    end
+end
+
+%   EXEMPLO:
+%Se Coluna = DH     ==>     1 0 0
+%Se Coluna = SL     ==>     0 1 0
+%Se Coluna = NO     ==>     0 0 1
+
+dataset = [colunasArquivo{1}, colunasArquivo{2}, colunasArquivo{3}, colunasArquivo{4}, colunasArquivo{5}, colunasArquivo{6}, colunasDeRotulos];
+
+%% Transforma vetores linha em coluna
+X = dataset(:, 1:6)';
+D = dataset(:, 7:9)';
+
+%% Normalização dos dados
+[n,~] = size(X);
+
+for i = 1 : n
+    x = X(i, :);
+    X(i, :) = (x - mean(x)) / std(x);
+end
+
+rede = feedforwardnet(40);% Gera um MLP com 40 neurônios ocultto
+
+%% Separa 30% das amostras para teste e o resto para treino
+amostrasTeste = 0.3;
+somaAcuracia = 0;
+
+%% EPOCAS:
+for epoca = 1 : 10 % Seta 10 épocas
+
+    [classes, amostras] = size(X);
+    [outputClasses, ~] = size(D);
+
+    qtdAmostrasTeste = round(amostrasTeste * amostras);
+
+    indicesAleatorio = randperm(length(X)); % Aleatorização dos indices
+
+    X_treino = X(:, indicesAleatorio);
+    Y_treino = D(:, indicesAleatorio);
+    
+    %Remove 30% das amostras de X_treino e as coloca em Y_teste
+    X_teste = zeros(classes, qtdAmostrasTeste);
+    Y_teste = zeros(outputClasses, qtdAmostrasTeste);
+    
+    for i = 1 : qtdAmostrasTeste
+        X_teste(:, i) = X_treino(:, i);
+        X_treino(:, i) = [];
+        Y_teste(:, i) = Y_treino(:, i);
+        Y_treino(:, i) = [];
+    end
+
+    rede = train(rede, X_treino, Y_treino); % Treinando o Modelo
+    
+    predicao = rede(X_teste); % Realizando uma predição
+
+    % Uma acurácia é obtida comparando os testes com a predição
+    [~, amostrasDeTeste] = size(Y_teste);
+
+    [~, classePred] = max(predicao);
+    [~, classeCerta] = max(Y_teste);
+
+    acertos = classePred == classeCerta;
+    acertosEmTeste = sum(acertos);
+    acuracia = acertosEmTeste / amostrasDeTeste;   
+
+    somaAcuracia = somaAcuracia + acuracia;
+    fprintf('Acurácia Época %d: %f\n', i, acuracia);
+
+end
+
+fprintf('Acurácia Média: %f\n', (somaAcuracia/10));
